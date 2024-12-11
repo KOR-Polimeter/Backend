1a93c8fd2d1c2633dbe63a949f5bcec21da15f81
DYNOV <dynovember05@naver.com>
2024-12-11 22:59:23 +0900

    delete feature

--- KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/HomeController.java
+++ KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/HomeController.java
@@ -18,65 +18,8 @@ import org.springframework.web.client.RestTemplate;
 @RequiredArgsConstructor
 @Controller
 public class HomeController {
-    private final KakaoService kakaoService;
-
     @GetMapping("/")
-    public String main(Model model) {
-        String kakaoUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=772c65da382eb8de1916b5771770e5e7&redirect_uri=http://kor-polimeter.info/auth/kakao/callback";
-        model.addAttribute("kakaoUrl", kakaoUrl);
-
+    public String main() {
         return "main";
-    }
-
-    @GetMapping("/login")
-    public String login(Model model) {
-        String kakaoUrl = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=772c65da382eb8de1916b5771770e5e7&redirect_uri=http://kor-polimeter.info/auth/kakao/callback";
-        model.addAttribute("kakaoUrl", kakaoUrl);
-
-        return "login";
-    }
-
-
-
-    @GetMapping("/auth/kakao/callback")
-    public String kakaoCallback(@RequestParam String code, Model model) throws Exception {
-        try {
-            KakaoDTO kakaoDTO = kakaoService.getKakaoInfo(code);
-            model.addAttribute("kakaoInfo", kakaoDTO.toString());
-            return "main"; // index.html로 이동
-        } catch (Exception e) {
-            e.printStackTrace();
-            model.addAttribute("error", "An error occurred");
-            return "main";
-        }
-    }
-
-    /*@GetMapping("/logout")
-    public String logout(HttpSession session) {
-        KakaoService.logoutUser(session.getId());
-        session.removeAttribute("kakaoInfo");
-        session.invalidate();
-        return "redirect:/index"; // 로그아웃 후 메인 페이지로 리다이렉트
-    }*/
-
-    @GetMapping("/logout")
-    public String kakaoLogout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-        if (authentication != null && authentication.getPrincipal() instanceof OidcUser) {
-            OidcUser oidcUser = (OidcUser) authentication.getPrincipal();
-            String accessToken = oidcUser.getIdToken().getTokenValue(); // 액세스 토큰 가져오기
-
-            // 카카오 로그아웃 API 호출
-            String kakaoLogoutUrl = "https://kapi.kakao.com/v1/user/logout";
-            RestTemplate restTemplate = new RestTemplate();
-            try {
-                restTemplate.postForEntity(kakaoLogoutUrl, null, String.class);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-        // Spring Security 로그아웃 처리
-        request.getSession().invalidate();
-        return "redirect:/";
     }
 }
