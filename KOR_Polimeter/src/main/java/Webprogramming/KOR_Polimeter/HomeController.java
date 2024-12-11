7b8867ba3251481799b5fc2dd1a024b4970d7360
DYNOV <dynovember05@naver.com>
2024-12-11 23:25:37 +0900

    test

--- KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/HomeController.java
+++ KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/HomeController.java
@@ -1,19 +1,8 @@
 package Webprogramming.KOR_Polimeter;
 
-import Webprogramming.KOR_Polimeter.dto.KakaoDTO;
-import Webprogramming.KOR_Polimeter.service.KakaoService;
-import Webprogramming.KOR_Polimeter.service.SearchService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.web.client.RestTemplate;
 
 @RequiredArgsConstructor
 @Controller
