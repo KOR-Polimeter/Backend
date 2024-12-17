0c3dbeca3a9e4aebf5d7b4b76daa70d0d7191e66
DY <79821041+2020114025-CPF21A@users.noreply.github.com>
2024-12-17 17:13:22 +0900

    Update Member.java

--- KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/domain/Member.java
+++ KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/domain/Member.java
@@ -5,15 +5,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 
 @Entity
 @Getter @Setter
 @NoArgsConstructor
 public class Member {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 명시적으로 설정
-    @Column(name = "member_id")
-    private long member_id;
-
-    private String username;
-    private String email;
+    @Column(name = "id") // AUTO_INCREMENT 컬럼에 해당하는 id
+    private String name; // 이름
+    private LocalDateTime bday; // 생일 (datetime 컬럼)
+    private String phone; // 전화번호
+    @Column(name = "created_at")
+    private LocalDateTime createdAt; // 생성일시 (datetime 컬럼)
+    private Integer gender; // 성별 (int 컬럼)
 }
