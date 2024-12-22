66db27c391e8f347b24fe0a344d509660674adbc
DYNOV <dynovember05@naver.com>
2024-12-23 00:00:15 +0900

    Update HomeController.java

--- KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/controller/HomeController.java
+++ KOR_Polimeter/src/main/java/Webprogramming/KOR_Polimeter/controller/HomeController.java
@@ -52,18 +52,6 @@ public class HomeController {
             // 카카오 API를 통해 사용자 정보 가져오기
             KakaoDTO kakaoDTO = kakaoService.getKakaoInfo(code);
 
-
-            // DB에서 userid를 기준으로 id(기본키) 조회
-            int dbId = kakaoService.findIdByUserId(kakaoDTO.getUserid());
-            System.out.println("디비 기본키인 id값 = " + dbId);
-
-                // 조회된 id를 kakaoDTO에 설정
-            kakaoDTO.setId(dbId);
-            System.out.println("DB에서 조회된 ID: " + dbId);
-
-            // 세션에 사용자 정보 저장
-            session.setAttribute("user", kakaoDTO);
-
             // 메인 페이지로 리다이렉트
             return "redirect:/";
         } catch (Exception e) {
